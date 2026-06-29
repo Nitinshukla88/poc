139fe2476e1e01a267ab3c3d3cdc9a8bf1da3054
Nitin Shukla <nitin.shukla12004@gmail.com>
2026-06-29 12:28:40 +0000

    modified api

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 import redis
-import json
+import json, os
 
 app = Flask(__name__)
-r = redis.Redis(host = 'redis', port = 6379, decode_responses = True)
+r = redis.Redis(os.environ.get("REDIS_HOST", "localhost"), port = 6379, decode_responses = True)
 
 @app.get('/tasks')
 def get_tasks():
@@ -12,7 +12,7 @@ def get_tasks():
 @app.post('/tasks')
 def post_tasks():
     task = request.json["title"]
-    r.rpush(tasks, json.dumps({"title" : task, "status" : "queued"}))
+    r.rpush("tasks", json.dumps({"title" : task, "status" : "queued"}))
     return jsonify({ "status" : "queued", "task" : task}), 201
 
 if __name__ == '__main__':
