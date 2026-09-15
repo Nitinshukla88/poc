0d6d43bd4961caf6f784c00c470e2d14e781fbf5
Nitin <nitin.shukla12004@gmail.com>
2026-09-15 09:10:09 +0000

    added version field into the endpoint

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def get_tasks():
 @app.get('/version')
 def get_version():
     return jsonify({ 
-            "message" : "Hi everyone, this is the version endpoint. Use it to check the version"
+                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v1"
     })
 @app.post('/tasks')
 def post_tasks():
