c70e13b09f2dfc81d5df37970d28a04f89212fa8
Nitin <nitin.shukla12004@gmail.com>
2026-09-20 21:28:33 +0000

    chore(): added small change again for loop prevention testing

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def get_tasks():
 @app.get('/version')
 def get_version():
     return jsonify({ 
-                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v4"
+                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v5"
     })
 @app.post('/tasks')
 def post_tasks():
