1e28f5225c12cbf82540c3c48ee6383c86e743c8
Nitin <nitin.shukla12004@gmail.com>
2026-09-20 20:58:38 +0000

    chore(): small change for webhook loop prevention testing

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def get_tasks():
 @app.get('/version')
 def get_version():
     return jsonify({ 
-                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v3"
+                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v4"
     })
 @app.post('/tasks')
 def post_tasks():
