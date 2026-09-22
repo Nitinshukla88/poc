60a9665470dfa1271da3727f9bd6dfd3f6042289
Nitin <nitin.shukla12004@gmail.com>
2026-09-22 11:52:40 +0000

    chore(): small change

--- api/app.py
+++ api/app.py
@@ -14,7 +14,7 @@ def get_tasks():
 @app.get('/version')
 def get_version():
     return jsonify({ 
-                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v77"
+                    "message" : "Hi everyone, this is the version endpoint. Use it to check the version", "version" : "v7"
     })
 @app.post('/tasks')
 def post_tasks():
